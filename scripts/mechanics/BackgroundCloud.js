8b18c19427b93b77f4dcf41c762f7c60eda5773a
soyeonrnehr <81903170+soyeonrnehr@users.noreply.github.com>
2021-04-14 22:55:27 +0900

    s

--- scripts/mechanics/BackgroundCloud.js
+++
@@ -1,8 +0,0 @@
-function BackgroundCloud(xpos,ypos,w,h,vel)
-{
-	this.x = xpos;
-	this.y = ypos;
-	this.width = w;
-	this.height = h;
-	this.velocity = vel;
-}
--- scripts/mechanics/Cloud.js
+++
@@ -1,9 +0,0 @@
-function Cloud(xpos, ypos)
-{
-	this.points = 5;
-	this.width = 50;
-	this.height = 20;
-	this.x = xpos;
-	this.y = ypos;
-	this.image = "";
-}
